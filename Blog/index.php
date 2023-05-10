a397e83e39ad688de8799df79b5d524aa38cb297
Mega cuteezzz <115076364+mega-rahmawati@users.noreply.github.com>
2023-05-10 17:44:57 +0700

    Add files via upload

---
+++ Blog/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Framework W3CSS</title>
+	<link rel="stylesheet" type="text/css" href="css/w3.css">
+</head>
+<body>
+	<div class="w3-container w3-blue">
+		<h1> My Blog </h1>
+	</div>	
+	<div class="w3-container">
+	<h1>Framework W3CSS</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	<div class="w3-container">
+		<h3>Sub Judul</h3>
+		<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<div class="w3-panel w3-pale-blue w3-leftbar w3-rightbar w3-border-blue">
+ 	 	<p>London is the most populous city in the United Kingdom,
+ 		 with a metropolitan area of over 9 million inhabitants.</p>
+		</div>
+	</div>
+	</div>
+	<div class="w3-container w3-yellow">
+		<h1>Judul Baru Lagi </h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<div class="w3-panel w3-red">
+			<h2>Peringatan!</h2>
+			<p>London is the most populous city in the United Kingdom, with a metropolitan area of over 9 million inhabitants.</p>
+		</div>
+	</div>
+
+	<div class="w3-container w3-gray">
+		<h5>My Footer</h5>
+	</div>
+</body>
+</html>
